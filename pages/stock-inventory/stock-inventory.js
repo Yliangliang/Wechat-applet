afaff9233fced7689575220d052f613e21fcb8b2
yuanliang <764278110@qq.com>
2019-05-18 23:06:40 +0800

    星期六提交

---
+++ pages/stock-inventory/stock-inventory.js
@@ -0,0 +1,72 @@
+// pages/stock-inventory/stock-inventory.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        cabinetNumber:'光谷创意1号柜',
+        date:'2019-03-08',
+        time:'11:25:24',
+        money:['7']
+
+
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
